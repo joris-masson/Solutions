9c219109af86624ff5334497fc1d30583c5fe69c
JojoAz1605 <joris.masson2002@gmail.com>
2021-09-17 14:59:54 +0200

    Rajout des commentaires pour repTel
    et un petit changement mineur

--- L2/POO/TP2/repTel/src/phonebook/Demo.java
+++ L2/POO/TP2/repTel/src/phonebook/Demo.java
@@ -3,13 +3,13 @@ package phonebook;
 public class Demo {
     public static void main(String[] args) {
         PhoneBook zePhoneBook = new PhoneBook();
-        zePhoneBook.addContact("moi", "0664501619");
+        zePhoneBook.addContact("moi", "0606060606");
         zePhoneBook.addContact("hen", "177013");
         zePhoneBook.addContact("hen", "228922");
-        zePhoneBook.addContact("Un imposteur", "0664501619");
+        zePhoneBook.addContact("Un imposteur", "0606060606");
 
         System.out.println(zePhoneBook.getPhoneNumbers("hen"));
-        System.out.println(zePhoneBook.getNames("0664501619"));
+        System.out.println(zePhoneBook.getNames("0606060606"));
 
     }
 }
--- L2/POO/TP2/repTel/src/phonebook/PhoneBook.java
+++ L2/POO/TP2/repTel/src/phonebook/PhoneBook.java
@@ -7,34 +7,34 @@ public class PhoneBook {
     ArrayList<Contact> contactList;
 
     public PhoneBook() {
-        this.contactList = new ArrayList<Contact>();
+        this.contactList = new ArrayList<Contact>();  // créé la liste des contacts
     }
 
     public void addContact(String name, String phoneNumber) {
-        Contact zeNewContact = new Contact(name, phoneNumber);
-        this.contactList.add(zeNewContact);
+        Contact zeNewContact = new Contact(name, phoneNumber);  // créé un nouveau contact
+        this.contactList.add(zeNewContact);  // ajoute le nouveau contact à la liste
     }
 
     public ArrayList<String> getPhoneNumbers(String name) {
-        ArrayList<String> correspond = new ArrayList<String>();
+        ArrayList<String> correspond = new ArrayList<String>();  // stocke les noms correspondants
 
-        for (int i = 0; i < this.contactList.size(); i++) {
-            if (this.contactList.get(i).getName() == name) {
-                correspond.add(this.contactList.get(i).getPhoneNumber());
+        for (int i = 0; i < this.contactList.size(); i++) {  // parcours la liste des contacts(méthode 1)
+            if (this.contactList.get(i).getName() == name) {  // si le nom du contact i est celui recherché
+                correspond.add(this.contactList.get(i).getPhoneNumber());  // l'ajoute aux correspondants
             }
         }
-        return correspond;
+        return correspond;  // retourne la liste
     }
 
     public ArrayList<String> getNames(String phoneNumber) {
-        ArrayList<String> correspond = new ArrayList<String>();
+        ArrayList<String> correspond = new ArrayList<String>();  // stocke les numéros correspondants
 
-        for (Contact contact : this.contactList) {
-            if (Objects.equals(contact.getPhoneNumber(), phoneNumber)) {
-                correspond.add(contact.getName());
+        for (Contact contact : this.contactList) {  // parcours les contacts(méthode 2)
+            if (Objects.equals(contact.getPhoneNumber(), phoneNumber)) {  // si le numéro est celui recherché
+                correspond.add(contact.getName());  // l'ajoute à la liste des correspondants
             }
         }
-        return correspond;
+        return correspond;  // retourne la liste
     }
 
 }
